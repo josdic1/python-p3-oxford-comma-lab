d6514276175c0c9aa3ed3ce9b4651db0cf3367a0
josdic1 <joshuadicker@gmail.com>
2025-05-13 14:28:28 -0400

    oxford comma done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,16 @@
 def oxford_comma(items):
-    return None
+  count = len(items)
+
+  if count == 1:
+    return(items[0])
+  if count == 2:
+    return(f"{items[0]} and {items[1]}")
+  elif count > 2:
+    return string_function(count, items)
+
+def string_function(num, items):
+  while num > 0:
+    for i in range(num):
+      last = items[-1]
+      rest = ", ".join(items[:-1])
+      return rest + ", and " + last
